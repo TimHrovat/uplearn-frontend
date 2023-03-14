836100f9f9fe18fe66cbb0c9d6b92aabd3ad5963
Tim Hrovat <tim.hrovat16@gmail.com>
2023-03-14 09:55:54 +0100

    loader fix

--- src/components/timetable/TimetablePage.tsx
+++ src/components/timetable/TimetablePage.tsx
@@ -2,14 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { AuthApi } from "../../api/auth/auth-api";
 import { UsersApi } from "../../api/users/users-api";
+import ErrorAlert from "../alerts/ErrorAlert";
+import Loader from "../Loader";
 import PageOutline from "../pages/PageOutline";
 import Timetable from "./Timetable";
 
 export default function TimetablePage() {
-  const { data: authUser } = useQuery({
+  const { status, data: authUser } = useQuery({
     queryKey: ["authenticatedUser"],
     queryFn: UsersApi.getAuthenticatedUser,
   });
+
+  if (status === "loading") return <Loader active={true} />;
+  if (status === "error") return <ErrorAlert msg={"Page couldn't load"} />;
 
   let timetable = <Timetable />;
 
